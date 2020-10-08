59feef561a5b7bedff33f8f1ec36d114693ad29d
W3G <55637907+w3gaucho@users.noreply.github.com>
2020-10-08 09:29:17 -0300

    select2 removido

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Buscar</title>
-    <link rel="stylesheet" href="css/select2.min.css">
     <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/select2.min.js"></script>
-    <script src="js/i18n/pt-BR.js"></script>
     <style>
     button{
         cursor:pointer;
@@ -17,7 +14,7 @@
     <div style="text-align:center;">
         <h1>Buscar</h1>
         <form class="" action="redirect.php" method="get">
-            <select id="caixaDeSites" name="key" class="js-example-basic-single">
+            <select name="key" style="width:400px">
                 <?php
                 require 'inc/erro.php';
                 $buscas=require 'inc/buscas.php';
@@ -33,30 +30,13 @@
                 }
                 ?>
             </select><br><br>
-            <textarea name="q" rows="8" cols="80" id="q"></textarea><br><br>
+            <input type="text" name="q" id="q" style="width:384px"><br><br>
             <button type="submit">Buscar</button>
         </form>
     </div>
     <script>
     $(function(){
-        //enter
-        $('textarea').on('keyup', function(e) {
-            if (e.which == 13 && ! e.shiftKey) {
-                $('textarea').val($('textarea').val().trim());
-                this.form.submit();
-            }
-        });
-        //fuzzy search
-        $('.js-example-basic-single').select2({
-            theme: "classic",
-            language: "pt-BR"
-        });
-        //focar no textarea
-        $('#caixaDeSites').select2('open');
-
-        $('#caixaDeSites').on('select2:select', function (e) { 
-            //$('textarea').focus();
-        });
+        $('input[type=text]').focus();
     });
 </script>
 </body>
